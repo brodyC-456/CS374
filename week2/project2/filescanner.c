b23ffeb7eace98569d593f87bb639848a015e813
brody couture <brodycouture05@gmail.com>
2026-01-17 15:59:24 -0800

    finished project 2 filescanner

--- week2/project2/filescanner.c
+++ week2/project2/filescanner.c
@@ -1,32 +1,94 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <dirent.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
+// Dealing with some magic numbers
+#define ERROR_CODE 0
+#define PATH_ARG_NUM 1
+#define PATH_BUF_SIZE PATH_MAX + 1
 
-int main(int argc, char *argv[]){
-    char* dir;
-    if (argc == 1){
-        dir = ".";
-    }
-    else if (argc == 2)
-    {
-        dir = argv[1];
-    }
-    else {
-        usage()
-    }
-
-    printf(count_bytes(dir));
-
+// Returns the stat struct we use for type and byte size
+struct stat get_lstat(char* dir_path){
+    struct stat buf;
+    lstat(dir_path, &buf);
+    return buf;
 }
 
-int count_bytes(dir){
-    printf("this will count the bytes at some point");
+// Handles Permission Errors when opening directories
+int file_read_error(char* pathname){
+    fprintf(stderr, "%s: failed to open\n", pathname);
+    return ERROR_CODE;
+}
+
+// Returns the size of a directory using its pathname
+int count_bytes(char* dir_path){
+
+    // Open Directory and Set byte_len
+    DIR *dir = opendir(dir_path);
+    if (dir == NULL){
+        return file_read_error(dir_path);
+    }
+    struct dirent *de;
+    int byte_len = 0;
+
+    // Main Loop, reads directory entries until it reaches the end of the directory (NULL)
+    while ((de = readdir(dir)) != NULL){
+        // Sets the specific file path we are opening (used in get_lstat and the recursive call)
+        char new_path[PATH_BUF_SIZE];
+        snprintf(new_path, sizeof(new_path), "%s/%s", dir_path, de->d_name);
+        struct stat buf = get_lstat(new_path);
+
+        // Use st_mode macros to determine file type
+        switch (buf.st_mode & S_IFMT){
+            // Add size of regular files to the total
+            case S_IFREG:
+                byte_len += buf.st_size;
+                break;
+            // Recurse into other directories, adding their size to the total
+            case S_IFDIR:
+                if(strcmp(de->d_name, ".") && strcmp(de->d_name, "..")){
+                    byte_len += count_bytes(new_path);
+                    break;
+                }
+                else{
+                    // Ignore .. and .
+                    break;
+                }
+            // Ignore all other file types
+            default:
+                break;
+        }
+    }
+    // Close the directory and print out the total byte_length
+    closedir(dir);
+    printf("%s: %d\n", dir_path, byte_len);
+    return byte_len;
 }
 
 void usage(void)
 {
-    fprintf(stderr, "usage: filescanner [directory]");
+    fprintf(stderr, "usage: filescanner [directory]\n");
     exit(1);
+}
+
+int main(int argc, char *argv[]){
+    char* dir_path;
+    if (argc == 1){
+        dir_path = ".";
+    }
+    else if (argc == 2)
+    {
+        dir_path = argv[PATH_ARG_NUM];
+    }
+    else {
+        usage();
+    }
+
+    count_bytes(dir_path);
 }
 
 
