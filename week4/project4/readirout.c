e086c278bada5360f57b3a696ea4006eeea257fe
brody couture <brodycouture05@gmail.com>
2026-01-28 12:53:36 -0800

    finished project 4

--- week4/project4/readirout.c
+++ week4/project4/readirout.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
 
+#define ERROR_CODE -1
+#define FILE_ARG_INDEX 1
+#define CMND_ARG_INDEX 2
 
 void usage(void)
 {
@@ -9,12 +13,33 @@ void usage(void)
     exit(1);
 }
 
+void add_command_output_to_file(char* filename, char* cmnd, char** args){
+    int fd;
+    if((fd = open(filename, O_CREAT|O_TRUNC|O_WRONLY, S_IRUSR | S_IWUSR)) == ERROR_CODE){
+        perror("file open error");
+        exit(1);
+    }
+
+    dup2(fd, 1);
+    if(execvp(cmnd, args) == ERROR_CODE){
+        perror("Invalid Command");
+        exit(1);
+    }
+}
+
 int main(int argc, char* argv[]){
+    char* filename;
+    char* cmnd;
+    char** args;
 
     if (argc < 3){
         usage();
     }
     else{
-        char* filename = argv[1]
+        filename = argv[FILE_ARG_INDEX];
+        cmnd = argv[CMND_ARG_INDEX];
+        args = &argv[CMND_ARG_INDEX];
     }
+
+    add_command_output_to_file(filename, cmnd, args);
 }
